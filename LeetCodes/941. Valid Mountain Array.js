bcf18759480799026d61dd16a013553b0cbde90f
Logan W <loganbryantweathersby@gmail.com>
2022-08-11 10:16:03 -0400

    formatting

--- LeetCodes/941. Valid Mountain Array.js
+++ LeetCodes/941. Valid Mountain Array.js
@@ -2,29 +2,26 @@
  * @param {number[]} arr
  * @return {boolean}
  */
- var validMountainArray = function(arr) {
-    let peak = Math.max(...arr)
-    let peakIndx = arr.indexOf(peak)
-    let peaked = false;
-    if(arr[peakIndx] === arr[0] || arr[peakIndx] === arr.at(-1)){
-        return false;
+var validMountainArray = function (arr) {
+  let peak = Math.max(...arr);
+  let peakIndx = arr.indexOf(peak);
+  let peaked = false;
+  if (arr[peakIndx] === arr[0] || arr[peakIndx] === arr.at(-1)) {
+    return false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[i + 1]) {
+      console.log("climbing");
     }
-    for(let i = 0; i < arr.length; i++){
-        
-      if(arr[i] < arr[i + 1])
-          {
-            console.log("climbing")  
-          }
-        if(peaked == true && arr[i] < arr[i + 1]){
-            return false
-        }
-        if(arr[i] > arr[i + 1])
-            {
-                console.log("descending")
-                peaked = true;
-            }
-        if(arr[i] == arr.at(-1)){
-            return true
-        }    
+    if (peaked == true && arr[i] < arr[i + 1]) {
+      return false;
     }
+    if (arr[i] > arr[i + 1]) {
+      console.log("descending");
+      peaked = true;
+    }
+    if (arr[i] == arr.at(-1)) {
+      return true;
+    }
+  }
 };
